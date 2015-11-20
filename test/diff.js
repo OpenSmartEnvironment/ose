15c5c34c06950aacaae27bb96b677809d3ad326e
Jan Snasel <snaselj@gmail.com>
2015-11-20 13:59:47 +0100

    Feature: Shard schemas

--- test/diff.js
+++ test/diff.js
@@ -6,8 +6,6 @@ exports = O.init('ose/lib/diff');
 
 var Assert = O.chai.assert
 
-var Diff = require('ose/lib/diff');
-
 // Tests {{{1
 exports.add('cleanPatch()', function(cb) {  // {{{2
   var val = {
@@ -21,7 +19,7 @@ exports.add('cleanPatch()', function(cb) {  // {{{2
     b: 2,
   };
 
-  Diff.cleanPatch(patch, val);
+  O.diff.cleanPatch(patch, val);
 
   Assert(O._.isEqual({a: 3}, patch), 'Patch');
 
